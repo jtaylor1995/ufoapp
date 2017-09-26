63da9be7ddf640f01fb16e7a143f22f3780696d5
James Taylor <j.taylor@kainos.com>
2017-09-26 12:03:15 +0100

    Draw circles from longs and lats

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var d3 = require("d3");
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', {
+    title: 'Express'});
 });
 
 module.exports = router;
